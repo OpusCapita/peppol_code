accd39027cb5b6ed2f6b1050b58acd5ad5613e62
Daniil Kalnin <daniil.kalnin@opuscapita.com>
2016-09-06 11:32:20 +0300

    Rewritten deserializing of PeppolEvent.

--- events-persistence/src/main/java/com/opuscapita/peppol/events/persistence/model/util/PeppolEventDeSerializer.java
+++ events-persistence/src/main/java/com/opuscapita/peppol/events/persistence/model/util/PeppolEventDeSerializer.java
@@ -2,11 +2,8 @@ package com.opuscapita.peppol.events.persistence.model.util;
 
 import com.google.gson.*;
 import com.opuscapita.peppol.events.persistence.model.PeppolEvent;
-import com.opuscapita.peppol.events.persistence.model.TransportType;
 
 import java.lang.reflect.Type;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Created by bambr on 16.5.9.
@@ -15,58 +12,15 @@ public class PeppolEventDeSerializer implements JsonDeserializer<PeppolEvent> {
 
     @Override
     public PeppolEvent deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        PeppolEvent result = new PeppolEvent();
-        populatePeppolEvent(jsonElement, result);
+        PeppolEvent result = fixSenderName(jsonElement);
         return result;
     }
 
-    protected void populatePeppolEvent(JsonElement jsonElement, PeppolEvent result) {
-        JsonObject rawJson = jsonElement.getAsJsonObject();
-        Map<String, Boolean> stringFields = new TreeMap<String, Boolean>() {{
-            put("commonName", false);
-            put("documentType", false);
-            put("dueDate", false);
-            put("errorFilePath", false);
-            put("errorMessage", false);
-            put("fileName", true);
-            put("invoiceDate", false);
-            put("invoiceId", false);
-            put("documentType", false);
-            put("recipientCountryCode", false);
-            put("recipientId", true);
-            put("recipientName", false);
-            put("senderCountryCode", false);
-            put("senderId", true);
-            put("senderName", false);
-            put("sendingProtocol", false);
-            put("transactionId", false);
-        }};
-        stringFields.entrySet().forEach(field -> populateFieldStringValue(result, rawJson, field.getKey(), field.getValue()));
-
-        populateFieldTransportTypeValue(result, rawJson, "transportType", true);
-
-        populateFieldLongValue(result, rawJson, "fileSize", true);
-    }
-
-    private void populateFieldLongValue(PeppolEvent result, JsonObject rawJson, String fieldName, Boolean mandatory) {
-        if (canPopulateField(rawJson, fieldName, mandatory)) {
-            result.setFileSize(rawJson.get(fieldName).getAsLong());
+    protected PeppolEvent fixSenderName(JsonElement jsonElement) {
+        JsonObject rawObject = jsonElement.getAsJsonObject();
+        if (!rawObject.has("senderName")) {
+            rawObject.addProperty("senderName", "n/a");
         }
-    }
-
-    private void populateFieldTransportTypeValue(PeppolEvent result, JsonObject rawJson, String fieldName, Boolean mandatory) {
-        if (canPopulateField(rawJson, fieldName, mandatory)) {
-            result.setTransportType(TransportType.valueOf(rawJson.get(fieldName).getAsString()));
-        }
-    }
-
-    private void populateFieldStringValue(PeppolEvent result, JsonObject rawJson, String fieldName, Boolean mandatory) {
-        if (canPopulateField(rawJson, fieldName, mandatory)) {
-            result.setCommonName(rawJson.get(fieldName).getAsString());
-        }
-    }
-
-    private boolean canPopulateField(JsonObject rawJson, String fieldName, Boolean mandatory) {
-        return mandatory && rawJson.has(fieldName);
+        return new Gson().fromJson(rawObject, PeppolEvent.class);
     }
 }
--- events-persistence/src/test/java/com/opuscapita/peppol/events/persistence/model/util/PeppolEventDeSerializerTest.java
+++ events-persistence/src/test/java/com/opuscapita/peppol/events/persistence/model/util/PeppolEventDeSerializerTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by bambr on 16.5.9.
@@ -18,7 +19,6 @@ public class PeppolEventDeSerializerTest {
     String[] fixtures = {
             "{}",
             "{\"transportType\":\"OUT_IN\",\"fileName\":\"ubl-40071810-6def-11e6-aad8-8f9310c3c4b8.xml\",\"fileSize\":0,\"invoiceId\":\"\",\"senderCountryCode\":\"\",\"recipientCountryCode\":\"\",\"invoiceDate\":\"\",\"dueDate\":\"\"}",
-            "{\"transportType\":\"IN_OUT\",\"fileName\":\"7ecd16db-bfc1-4374-9be7-3ec00a56e9f8.xml\",\"fileSize\":38497,\"invoiceId\":\"356042\",\"senderId\":\"9908:919095105\",\"senderName\":\"LAKS-- VILDTCENTRALEN AS\",\"senderCountryCode\":\"NO\",\"recipientId\":\"9908:994356550\",\"recipientName\":\"NIWA\",\"recipientCountryCode\":\"NO\",\"invoiceDate\":\"2016-05-31\",\"dueDate\":\"2016-06-21\",\"transactionId\":\"7ecd16db-bfc1-4374-9be7-3ec00a56e9f8\",\"documentType\":\"Invoice\",\"commonName\":\"O\\u003dEVRY Norge AS,CN\\u003dAPP_1000000148,C\\u003dNO\",\"sendingProtocol\":\"AS2\"}"
     };
     private PeppolEventDeSerializer testObject;
 
@@ -40,9 +40,8 @@ public class PeppolEventDeSerializerTest {
     }
 
     protected void testPopulateEventWithFixture(String fixture) {
-        PeppolEvent testPeppolEvent = new PeppolEvent();
-        testObject.populatePeppolEvent(new Gson().fromJson(fixture, JsonElement.class), testPeppolEvent);
-        System.out.println(testPeppolEvent);
+        PeppolEvent testPeppolEvent = testObject.fixSenderName(new Gson().fromJson(fixture, JsonElement.class).getAsJsonObject());
+        assertNotNull(testPeppolEvent.getSenderName());
         assertEquals("n/a", testPeppolEvent.getSenderName());
     }
 
