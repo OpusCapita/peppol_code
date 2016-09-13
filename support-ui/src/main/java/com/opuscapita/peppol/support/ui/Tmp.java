919f47193749936c50797b870db58d90fa718f94
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2016-09-13 15:48:16 +0300

    Initial commit for support-ui

---
+++ support-ui/src/main/java/com/opuscapita/peppol/support/ui/Tmp.java
@@ -0,0 +1,7 @@
+package com.opuscapita.peppol.support.ui;
+
+/**
+ * Created by gamanse1 on 2016.09.13..
+ */
+public class Tmp {
+}
---
+++ support-ui/src/test/java/Tmp.java
@@ -0,0 +1,5 @@
+/**
+ * Created by gamanse1 on 2016.09.13..
+ */
+public class Tmp {
+}
