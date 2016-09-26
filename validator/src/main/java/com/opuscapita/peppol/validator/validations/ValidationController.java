de0a3388124c4793bd01db37447d5dab62eaf12a
Daniil Kalnin <daniil.kalnin@opuscapita.com>
2016-09-26 03:53:04 +0300

    Work in progress on validator.

--- validator/src/main/java/com/opuscapita/peppol/validator/validations/ValidationController.java
+++ validator/src/main/java/com/opuscapita/peppol/validator/validations/ValidationController.java
@@ -1,12 +1,22 @@
 package com.opuscapita.peppol.validator.validations;
 
 import com.opuscapita.peppol.commons.container.ContainerMessage;
+import com.opuscapita.peppol.commons.container.document.impl.Archetype;
 import com.opuscapita.peppol.validator.validations.common.BasicValidator;
 import com.opuscapita.peppol.validator.validations.common.ValidationResult;
+import com.opuscapita.peppol.validator.validations.common.util.ValidationErrorBuilder;
 import com.opuscapita.peppol.validator.validations.difi.DifiValidator;
+import com.opuscapita.peppol.validator.validations.svefaktura1.SveFaktura1Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by bambr on 16.5.8.
@@ -18,24 +28,61 @@ public class ValidationController {
     ApplicationContext context;
 
     public ValidationResult validate(ContainerMessage containerMessage) {
-        String profileId = containerMessage.getDocument().getProfileId();
-        String archetype = extractArchetype(profileId);
-        BasicValidator validator;
+        Archetype archetype = containerMessage.getDocument().getArchetype();
+        BasicValidator validator = getValidator(archetype);
+        ValidationResult result = new ValidationResult(containerMessage.getDocument().getArchetype());
+        result.setPassed(false);
+        if (validator != null) {
+            byte[] data = new byte[0];
+            try {
+                data = getDocumentBytes(containerMessage);
+            } catch (TransformerException e) {
+                e.printStackTrace();
+                addNonTypicalErrorToValidationResult(result, "Validation failed on transforming XML to byte array", e.getMessage());
+            }
+            result.setPassed(validator.validate(data));
+            if (!result.isPassed()) {
+                validator.getErrors().forEach(result::addError);
+            }
+        } else {
+            addNonTypicalErrorToValidationResult(result, "Failed to get validator", "No validator found for archetype: " + archetype);
+        }
+        return result;
+    }
+
+    private BasicValidator getValidator(Archetype archetype) {
+        BasicValidator validator = null;
         switch (archetype) {
-            case "SVEFAKTURA1":
+            case SVEFAKTURA1:
+                validator = context.getBean("svefaktura1", SveFaktura1Validator.class);
+                break;
+            case INVALID:
 
                 break;
+            case UBL:
             default:
                 validator = context.getBean("difi", DifiValidator.class);
         }
-        //validator.validate(containerMessage) //get byte[] out of Document type
-        return null;
+        return validator;
     }
 
-    private String extractArchetype(String profileId) {
-        if(profileId.toLowerCase().contains("urn:sfti:documents:BasicInvoice:1:0")) {
-            return "SVEFAKTURA1";
-        }
-        return null;
+    private void addNonTypicalErrorToValidationResult(ValidationResult validationResult, String title, String details) {
+        validationResult.addError(
+                ValidationErrorBuilder
+                        .aValidationError()
+                        .withTitle(title)
+                        .withDetails(details)
+                        .build()
+        );
+    }
+
+    private byte[] getDocumentBytes(ContainerMessage containerMessage) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(containerMessage.getDocument().getDocument());
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        StreamResult result = new StreamResult(bos);
+        transformer.transform(source, result);
+        return bos.toByteArray();
     }
 }
