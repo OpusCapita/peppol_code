9cc276a4615fd9ffa69f66eec9610d23bb198a1f
gamanse1 <sergejs.gamans@opuscapita.com>
2016-11-23 16:19:44 +0200

    testing

--- smoke-tests/src/main/java/com/opuscapita/peppol/test/tools/smoke/checks/subtypes/QueuesCheck.java
+++ smoke-tests/src/main/java/com/opuscapita/peppol/test/tools/smoke/checks/subtypes/QueuesCheck.java
@@ -5,6 +5,8 @@ import com.opuscapita.peppol.test.tools.smoke.checks.CheckResult;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.Map;
 
@@ -13,6 +15,7 @@ import java.util.Map;
  */
 public class QueuesCheck extends Check {
 
+    private final static Logger logger = LogManager.getLogger(QueuesCheck.class);
     final String _mesage = "Smoke Test MQ message";
 
     public QueuesCheck(String moduleName, Map<String, String> params) {
@@ -26,6 +29,7 @@ public class QueuesCheck extends Check {
         CheckResult checkResult = null;
         String[] queues = rawConfig.get("queue").trim().split(" ");
         try{
+            logger.info("QueuesCheck: Start");
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost(rawConfig.get("host"));
             factory.setPort((int)(Object)rawConfig.get("port"));
@@ -34,19 +38,24 @@ public class QueuesCheck extends Check {
             factory.setConnectionTimeout(500);
             connection = factory.newConnection();
             channel = connection.createChannel();
-
             for(String queue : queues) {
                 channel.queueDeclare(queue, true, false, false, null);
                 channel.basicPublish("", queue, null, _mesage.getBytes("UTF-8"));
             }
-
-            channel.close();
-            connection.close();
             checkResult = new CheckResult(name, true, "Queues check succeeded ", rawConfig);
-
         } catch (Exception ex){
             ex.printStackTrace();
             checkResult =  new CheckResult(name, false, "Queues check failed " + ex, rawConfig);
+        }
+        finally {
+            try{
+                if (channel != null)
+                    channel.close();
+                if(connection != null)
+                    connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return checkResult;
     }
