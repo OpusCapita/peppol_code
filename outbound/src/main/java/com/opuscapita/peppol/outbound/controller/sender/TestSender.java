53fa262c45295252fccc2d18e03459363f7f04e2
Sergejs Roze <klopojed@yahoo.de>
2017-01-20 13:28:51 +0000

    Implemented TestSender with selectable sender/recipient id

--- outbound/src/main/java/com/opuscapita/peppol/outbound/controller/sender/TestSender.java
+++ outbound/src/main/java/com/opuscapita/peppol/outbound/controller/sender/TestSender.java
@@ -8,6 +8,7 @@ import eu.peppol.identifier.PeppolProcessTypeId;
 import eu.peppol.outbound.transmission.*;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * More advanced test sender that the {@link FakeSender}.
@@ -33,11 +32,11 @@ public class TestSender extends UblSender {
 
     private final FakeSender fakeSender;
 
-    @Value("${peppol.outbound.test.recipient:''}")
+    @Value("${peppol.outbound.test.recipient:}")
     private String testRecipient;
 
     @Autowired
-    public TestSender(OxalisOutboundModuleWrapper oxalisOutboundModuleWrapper, FakeSender fakeSender) {
+    public TestSender(@Nullable OxalisOutboundModuleWrapper oxalisOutboundModuleWrapper, @NotNull FakeSender fakeSender) {
         super(oxalisOutboundModuleWrapper);
         this.fakeSender = fakeSender;
     }
@@ -71,7 +70,7 @@ public class TestSender extends UblSender {
                     .sender(new ParticipantId(document.getSenderId()))
                     .receiver(new ParticipantId(testRecipient))
                     .trace(true)
-                    .payLoad(inputStream);
+                    .payLoad(getUpdatedFileContent(cm, testRecipient));
 
             TransmissionRequest transmissionRequest = requestBuilder.build();
 
@@ -79,6 +78,28 @@ public class TestSender extends UblSender {
 
             return transmitter.transmit(transmissionRequest);
         }
+    }
 
+    @SuppressWarnings("ConstantConditions")
+    InputStream getUpdatedFileContent(@NotNull ContainerMessage cm, @NotNull String fakeId) throws IOException {
+        long sizeL = new File(cm.getFileName()).length();
+        int sizeI = sizeL > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) sizeL;
+        org.apache.commons.io.output.ByteArrayOutputStream result = new org.apache.commons.io.output.ByteArrayOutputStream(sizeI);
+
+        String senderId = cm.getBaseDocument().getSenderId();
+        String recipientId = cm.getBaseDocument().getRecipientId();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(cm.getFileName()))) {
+            String line = reader.readLine();
+            while (line != null) {
+                line = StringUtils.replace(line, senderId, fakeId);
+                line = StringUtils.replace(line, recipientId, fakeId);
+                result.write(line.getBytes());
+                result.write("\n".getBytes());
+                line = reader.readLine();
+            }
+        }
+
+        return new ByteArrayInputStream(result.toByteArray());
     }
 }
