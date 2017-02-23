8c6a2d56ecdc84265b83a6596a7dd2d3d5c20c8b
Daniil Kalnin <daniil.kalnin@opuscapita.com>
2017-02-23 09:46:34 +0200

    Removed check for sender/receiver consistency in ValidationController, since such check is performed on much lower level now.

--- validator/src/main/java/com/opuscapita/peppol/validator/validations/ValidationController.java
+++ validator/src/main/java/com/opuscapita/peppol/validator/validations/ValidationController.java
@@ -8,7 +8,6 @@ import com.opuscapita.peppol.commons.validation.BasicValidator;
 import com.opuscapita.peppol.commons.validation.ValidationError;
 import com.opuscapita.peppol.commons.validation.ValidationResult;
 import com.opuscapita.peppol.validator.validations.common.SbdhValidator;
-import com.opuscapita.peppol.validator.validations.common.SenderReceiverConsistencyValidator;
 import com.opuscapita.peppol.validator.validations.common.ValidatorFactory;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -35,13 +34,11 @@ public class ValidationController {
 
     private final SbdhValidator sbdhValidator;
     private final ValidatorFactory validatorFactory;
-    private final SenderReceiverConsistencyValidator senderReceiverConsistencyValidator;
 
     @Autowired
-    public ValidationController(@NotNull SbdhValidator sbdhValidator, @NotNull ValidatorFactory validatorFactory, SenderReceiverConsistencyValidator senderReceiverConsistencyValidator) {
+    public ValidationController(@NotNull SbdhValidator sbdhValidator, @NotNull ValidatorFactory validatorFactory) {
         this.sbdhValidator = sbdhValidator;
         this.validatorFactory = validatorFactory;
-        this.senderReceiverConsistencyValidator = senderReceiverConsistencyValidator;
     }
 
     public ValidationResult validate(@NotNull ContainerMessage containerMessage) {
@@ -75,16 +72,8 @@ public class ValidationController {
         }
 
         ValidationResult result = new ValidationResult(containerMessage.getBaseDocument().getArchetype());
-        result.setPassed(true);
-
-        if(!archetype.equals(Archetype.SVEFAKTURA1) && !archetype.equals(Archetype.INVALID)) {
-            ValidationResult senderReceiverConsistencyCheckResult = senderReceiverConsistencyValidator.senderAndReceiverAreSame(containerMessage);
-            if(!senderReceiverConsistencyCheckResult.isPassed()) {
-                result.setPassed(false);
-                senderReceiverConsistencyCheckResult.getErrors().forEach(result::addError);
-            }
-        }
-
+        result.setPassed(false);
+        
         byte[] data;
         try {
             data = DocumentContentUtils.getDocumentBytes(getRootDocument(containerMessage));
@@ -94,7 +83,7 @@ public class ValidationController {
 
         List<ValidationError> sbdhValidationErrors = sbdhValidator.performXsdValidation(containerMessage);
         ValidationResult validatorResult = validator.validate(data);
-        result.setPassed(result.isPassed() && validatorResult.isPassed() && sbdhValidationErrors.size() == 0);
+        result.setPassed(validatorResult.isPassed() && sbdhValidationErrors.size() == 0);
         if (!result.isPassed()) {
             sbdhValidationErrors.forEach(result::addError);
             validatorResult.getErrors().forEach(result::addError);
