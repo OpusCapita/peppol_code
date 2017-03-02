07a486011f1ca2d4c1a0479ca8942321db5d858a
rozeser1 <sergejs.roze@opuscapita.com>
2017-03-02 16:49:05 +0200

    Fixed typo in Edgars name

--- common/src/main/java/com/opuscapita/peppol/commons/errors/PeppolFailureAnalyser.java
+++ common/src/main/java/com/opuscapita/peppol/commons/errors/PeppolFailureAnalyser.java
@@ -10,8 +10,8 @@ public class PeppolFailureAnalyser extends AbstractFailureAnalyzer {
 
     @Override
     protected FailureAnalysis analyze(Throwable rootFailure, Throwable cause) {
-        if(cause.getMessage().contains("Could not resolve placeholder")) {
-            return new FailureAnalysis(cause.getMessage(), "Please, ask Edagrs to add it to configuration (and give him some chocolate cookies).", cause);
+        if (cause.getMessage().contains("Could not resolve placeholder")) {
+            return new FailureAnalysis(cause.getMessage(), "Please, ask Edgars to add it to configuration (and give him some chocolate cookies).", cause);
         }
         return null;
     }
