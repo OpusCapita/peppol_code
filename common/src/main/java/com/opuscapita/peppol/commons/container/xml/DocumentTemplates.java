036494ecf38772e0f5424ff942c4945247869b36
Sergejs Roze <klopojed@yahoo.de>
2017-04-04 21:57:41 +0300

    Fixing issue with failing format recognition

--- common/src/main/java/com/opuscapita/peppol/commons/container/xml/DocumentTemplates.java
+++ common/src/main/java/com/opuscapita/peppol/commons/container/xml/DocumentTemplates.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
@@ -18,7 +17,6 @@ import java.util.List;
  * @author Sergejs.Roze
  */
 @Component
-@Lazy
 public class DocumentTemplates {
     private final static Logger logger = LoggerFactory.getLogger(DocumentTemplates.class);
 
