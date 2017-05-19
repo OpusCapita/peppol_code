5ca856427e9910b8523347554cca150810c71d63
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2017-05-19 15:37:26 +0300

    cleaning done, removed logs used for debugging

--- common/src/main/java/com/opuscapita/peppol/commons/container/xml/DocumentParserHandler.java
+++ common/src/main/java/com/opuscapita/peppol/commons/container/xml/DocumentParserHandler.java
@@ -128,12 +128,9 @@ public class DocumentParserHandler extends DefaultHandler {
         if (templates.size() == 0) {
             return noResult(null);
         }
-        logger.warn("templates.size() -> " + templates.size()); //TODO remove
         // remove all templates without matched root
         Template best = templates.stream().max(Comparator.comparingInt(t -> t.matchedCount)).orElse(null);
-        logger.warn("best template != null -> " + (best != null));
         templates.removeIf(template -> ":".equals(template.rootNameSpace));
-        logger.warn("templates.size() -> " + templates.size());
         if (templates.size() == 0) {
             return noResult(best);
         }
@@ -166,7 +163,6 @@ public class DocumentParserHandler extends DefaultHandler {
     }
 
     private DocumentInfo oneResult(Template template) {
-        logger.warn("private DocumentInfo oneResult called !"); //TODO remove
         DocumentInfo result = new DocumentInfo();
         result.setRootNodeName(template.originalRoot);
         result.setRootNameSpace(template.rootNameSpace);
