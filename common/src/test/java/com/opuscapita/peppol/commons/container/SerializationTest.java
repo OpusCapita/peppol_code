60ba72643b64fba6243bb407a3f152f1a3b6b088
rozeser1 <Klop0317>
2017-05-24 12:25:57 +0300

    Made serialization unit test great again

--- common/src/test/java/com/opuscapita/peppol/commons/container/SerializationTest.java
+++ common/src/test/java/com/opuscapita/peppol/commons/container/SerializationTest.java
@@ -1,6 +1,18 @@
 package com.opuscapita.peppol.commons.container;
 
+import com.opuscapita.peppol.commons.container.process.route.Endpoint;
+import com.opuscapita.peppol.commons.container.process.route.ProcessType;
+import com.opuscapita.peppol.commons.container.process.route.Route;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Sergejs.Roze
@@ -11,39 +23,38 @@ public class SerializationTest {
     @SuppressWarnings("ConstantConditions")
     @Test
     public void testSerialization() throws Exception {
-//        Endpoint endpoint = new Endpoint("test", ProcessType.TEST);
-//
-//        ContainerMessage cm = new ContainerMessage("metadata", "filename1", new Endpoint("test", ProcessType.OUT_FILE_TO_MQ));
-//        cm.setTransactionId("666");
-//        cm.setStatus(endpoint, "result");
-//
-//        Route route = new Route();
-//        route.setEndpoints(Arrays.asList("a", "b", "c"));
-//        route.setDescription("description");
-//        route.setMask("mask");
-//        route.setSource("test");
-//        cm.setRoute(route);
-//
-//        try (InputStream is = SerializationTest.class.getResourceAsStream("/valid/ehf.xml")) {
-//            cm.setDocumentInfo(new DocumentLoader().load(is, "filename2"));
-//        }
-//
-//        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-//        ObjectOutputStream oos = new ObjectOutputStream(buffer);
-//        oos.writeObject(cm);
-//
-//        ContainerMessage result = (ContainerMessage) new ObjectInputStream(new ByteArrayInputStream(buffer.toByteArray())).readObject();
-//
-//        assertEquals("test", result.getProcessingStatus().getEndpoint().getName());
-//        assertEquals("result", result.getProcessingStatus().getResult());
-//        assertEquals("metadata", result.getSourceMetadata());
-//        assertEquals("filename1", result.getFileName());
-//        assertEquals(new Endpoint("test", ProcessType.OUT_FILE_TO_MQ), result.getSource());
-//        assertEquals("a", result.getRoute().pop());
-//        assertEquals("description", result.getRoute().getDescription());
-//        assertEquals("mask", result.getRoute().getMask());
-//        assertEquals("test", result.getRoute().getSource());
-//        assertNotNull(result.getDocumentInfo());
+        Endpoint endpoint = new Endpoint("test", ProcessType.TEST);
+
+        ContainerMessage cm = new ContainerMessage("metadata", "filename1", new Endpoint("test", ProcessType.OUT_FILE_TO_MQ));
+        cm.getProcessingInfo().setTransactionId("666");
+        cm.setStatus(endpoint, "result");
+
+        Route route = new Route();
+        route.setEndpoints(Arrays.asList("a", "b", "c"));
+        route.setDescription("description");
+        route.setMask("mask");
+        route.setSource("test");
+        cm.getProcessingInfo().setRoute(route);
+
+        DocumentInfo di = new DocumentInfo();
+        cm.setDocumentInfo(di);
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(buffer);
+        oos.writeObject(cm);
+
+        ContainerMessage result = (ContainerMessage) new ObjectInputStream(new ByteArrayInputStream(buffer.toByteArray())).readObject();
+
+        assertEquals("test", result.getProcessingInfo().getCurrentEndpoint().getName());
+        assertEquals("result", result.getProcessingInfo().getCurrentStatus());
+        assertEquals("metadata", result.getProcessingInfo().getSourceMetadata());
+        assertEquals("filename1", result.getFileName());
+        assertEquals(new Endpoint("test", ProcessType.OUT_FILE_TO_MQ), result.getProcessingInfo().getSource());
+        assertEquals("a", result.getProcessingInfo().getRoute().pop());
+        assertEquals("description", result.getProcessingInfo().getRoute().getDescription());
+        assertEquals("mask", result.getProcessingInfo().getRoute().getMask());
+        assertEquals("test", result.getProcessingInfo().getRoute().getSource());
+        assertNotNull(result.getDocumentInfo());
     }
 
 }
