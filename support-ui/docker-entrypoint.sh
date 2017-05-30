1bc9c80b96a9ada26f885778ea4ef5f92a681d60
Didrihsons Edgars <edgars.didrihsons@opuscapita.com>
2017-05-30 16:50:17 +0300

    Prepare support-ui for prod deployment

---
+++ support-ui/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#set -x
+
+export MYSQL_HEALTH="http://mysql.docker.local:3306"
+export RABBITMQ_HEALTH="http://rabbitmq.docker.local:15672"
+
+# change from default umask of 0022 to allow group writes
+umask 0007
+
+# SIGTERM-handler
+term_handler() {
+  echo "SIGTERM received"
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# SIGINT-handler
+int_handler() {
+  echo "SIGINT received"
+  exit 130; # 128 + 2 -- SIGINT
+}
+
+# register signal handlers
+trap 'term_handler' SIGTERM
+trap 'int_handler' SIGINT
+
+# first parameter '-jar' indicates regular startup
+if [[ "$1" == "catalina.sh" ]]; then
+  # check services before startup
+  while ! curl -fs ${MYSQL_HEALTH} > /dev/null; do
+      echo "$(date) - still waiting on ${MYSQL_HEALTH}";
+      sleep 1;
+  done
+
+  while ! curl -fs ${RABBITMQ_HEALTH} > /dev/null; do
+      echo "$(date) - still waiting on ${RABBITMQ_HEALTH}";
+      sleep 1;
+  done
+
+  exec "$@"
+fi
+
+# if there are params the user is likely trying to explore the image
+exec "$@"
