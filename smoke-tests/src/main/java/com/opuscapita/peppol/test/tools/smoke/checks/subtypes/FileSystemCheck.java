dee16e45b9a47042a55c6bf580acec4577f7b79b
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2017-07-11 14:07:41 +0300

    more logging and results for file system check

--- smoke-tests/src/main/java/com/opuscapita/peppol/test/tools/smoke/checks/subtypes/FileSystemCheck.java
+++ smoke-tests/src/main/java/com/opuscapita/peppol/test/tools/smoke/checks/subtypes/FileSystemCheck.java
@@ -5,6 +5,7 @@ import com.opuscapita.peppol.test.tools.smoke.checks.CheckResult;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,22 +24,35 @@ public class FileSystemCheck extends Check {
             boolean canWrite = true, canRead = true;
             List<String> writableDirectories = ((List<String>)rawConfig.get("writable-directories"));
             List<String> readableDirectories = ((List<String>)rawConfig.get("readable-directories"));
-
+            List<String> writeErrors = new ArrayList<>();
+            List<String> readErrors = new ArrayList<>();
             for (String dir: writableDirectories) {
-                if (!Files.isWritable(new File(dir).toPath()))
+                if(! new File(dir).exists()){
+                    writeErrors.add("Error: unable to locate directory " + dir);
                     canWrite = false;
+                }
+                if (!Files.isWritable(new File(dir).toPath())){
+                    writeErrors.add("Error: can't write to directory " + dir);
+                    canWrite = false;
+                }
             }
              for (String dir : readableDirectories){
-                 if (!Files.isReadable(new File(dir).toPath()))
+                 if(! new File(dir).exists()){
+                     readErrors.add("Error: unable to locate directory " + dir);
                      canRead = false;
+                 }
+                 if (!Files.isReadable(new File(dir).toPath())) {
+                     readErrors.add("Error: can't read  directory " + dir);
+                     canRead = false;
+                 }
              }
 
-            return new CheckResult(moduleName, canWrite && canRead, "read/write check for directories: " +
+            return new CheckResult(moduleName, canWrite && canRead, writeErrors + " " + readErrors + " read/write check for directories: " +
                     rawConfig.get("readable-directories") + " and " +
                     rawConfig.get("writable-directories"), rawConfig);
         } catch (Exception ex){
             ex.printStackTrace();
-            return new CheckResult(moduleName, false, "read/write check for directories: " +
+            return new CheckResult(moduleName, false,"read/write check for directories: " +
                     rawConfig.get("readable-directories") + " and " +
                     rawConfig.get("writable-directories") + " failed! " + ex, rawConfig);
         }
