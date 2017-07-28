2ffde0dc863fffdbd5cbe44375886887739f151a
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2017-07-28 15:38:42 +0300

    added delay with retry for MLR test, because eventing is slow

--- integration-tests/src/main/java/com/opuscapita/peppol/test/tools/integration/consumers/subtypes/FileConsumer.java
+++ integration-tests/src/main/java/com/opuscapita/peppol/test/tools/integration/consumers/subtypes/FileConsumer.java
@@ -2,6 +2,7 @@ package com.opuscapita.peppol.test.tools.integration.consumers.subtypes;
 
 import com.opuscapita.peppol.test.tools.integration.consumers.Consumer;
 import com.opuscapita.peppol.test.tools.integration.test.TestResult;
+import org.apache.log4j.LogManager;
 
 import java.io.File;
 
@@ -9,8 +10,10 @@ import java.io.File;
  * Created by gamanse1 on 2016.11.24..
  */
 public class FileConsumer extends Consumer {
+    private final static org.apache.log4j.Logger logger = LogManager.getLogger(FileConsumer.class);
     private final String name;
     private final String expectedValue;
+    private final static int DELAY = 4000;
 
     public FileConsumer(String id, String name, String expectedValue) {
         super(id);
@@ -30,9 +33,19 @@ public class FileConsumer extends Consumer {
             return new TestResult(name, false, "FileConsumer: Invalid consumable, null or empty!");
         File directory = (File) consumable;
         if(!directory.isDirectory())
-            return new TestResult(name, false, "FileConsumer Directory not found " + directory);
-        if(directory.list().length < 1)
+            return new TestResult(name, false, "FileConsumer: Directory not found " + directory);
+        if(directory.list().length < 1){
+            logger.warn("FileConsumer: no files to consume in " + directory + " retry in: " + DELAY);
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if(directory.list().length < 1) {
+            logger.warn("FileConsumer: still no files to consume in " + directory);
             return new TestResult(name, false, "FileConsumer: no files to consume in " + directory);
+        }
         for (File f : directory.listFiles()){
             if (f.getName().equals(expectedValue)){
                 testResult = new TestResult(name, true, "Found expected file " + f.getAbsolutePath());
