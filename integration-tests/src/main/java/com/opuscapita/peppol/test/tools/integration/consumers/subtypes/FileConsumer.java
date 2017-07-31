950bd52a8a965e28638afb5588cb82f7dccd873c
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2017-07-31 17:11:43 +0300

    added additional delay for retry, reworked file searching logic

--- integration-tests/src/main/java/com/opuscapita/peppol/test/tools/integration/consumers/subtypes/FileConsumer.java
+++ integration-tests/src/main/java/com/opuscapita/peppol/test/tools/integration/consumers/subtypes/FileConsumer.java
@@ -13,7 +13,7 @@ public class FileConsumer extends Consumer {
     private final static org.apache.log4j.Logger logger = LogManager.getLogger(FileConsumer.class);
     private final String name;
     private final String expectedValue;
-    private final static int DELAY = 4000;
+    private final static int DELAY = 6000;
 
     public FileConsumer(String id, String name, String expectedValue) {
         super(id);
@@ -34,7 +34,8 @@ public class FileConsumer extends Consumer {
         if(!directory.isDirectory())
             return new TestResult(name, false, "FileConsumer: Directory not found " + directory);
         TestResult testResult = new TestResult(name, false, "FileConsumer: expected file: " + expectedValue + " not found in: " + directory);
-        if(directory.list().length < 1){
+        File f = new File(directory,expectedValue);
+        if(!f.exists()){
             logger.warn("FileConsumer: no files to consume in " + directory + " retry in: " + DELAY);
             try {
                 Thread.sleep(DELAY);
@@ -42,15 +43,9 @@ public class FileConsumer extends Consumer {
                 e.printStackTrace();
             }
         }
-        if(directory.list().length < 1) {
-            logger.warn("FileConsumer: still no files to consume in " + directory);
-            return testResult;
-        }
-        for (File f : directory.listFiles()){
-            if (f.getName().equals(expectedValue)){
-                f.delete();
-                return new TestResult(name, true, "Found expected file " + f.getAbsolutePath());
-            }
+        if(f.exists()) {
+            f.delete();
+            return new TestResult(name, true, "Found expected file " + f.getAbsolutePath());
         }
         return testResult;
     }
