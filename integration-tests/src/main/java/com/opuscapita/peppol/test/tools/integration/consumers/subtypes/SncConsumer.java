78b6fe82a575cfe3f3d062ab71b3177eace72fac
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2017-08-03 17:56:28 +0300

    SNC test here

--- integration-tests/src/main/java/com/opuscapita/peppol/test/tools/integration/consumers/subtypes/SncConsumer.java
+++ integration-tests/src/main/java/com/opuscapita/peppol/test/tools/integration/consumers/subtypes/SncConsumer.java
@@ -1,6 +1,5 @@
 package com.opuscapita.peppol.test.tools.integration.consumers.subtypes;
 
-import com.opuscapita.peppol.test.tools.integration.consumers.Consumer;
 import com.opuscapita.peppol.test.tools.integration.test.TestResult;
 
 import java.io.File;
@@ -8,32 +7,30 @@ import java.io.File;
 /**
  * Created by gamanse1 on 2016.11.24..
  */
-public class SncConsumer extends Consumer {
-
-
-    private final String name;
-    private final String expected;
+public class SncConsumer extends FileConsumer {
 
     public SncConsumer(String id, String name, String expected) {
-        super(id);
-        this.name = name;
-        this.expected = expected;
-    }
-
-    @Override
-    public boolean isDone() {
-        return false;
+        super(id, name, expected);
     }
 
     @Override
     public TestResult consume(Object consumable) {
-        if(consumable == null)
+        if(consumable == null) {
             return new TestResult(name, false, "SncConsumer: Invalid consumable, null or empty!");
+        }
+
         File directory = (File) consumable;
-        if(!directory.isDirectory() || directory.list().length < 1)
-            return new TestResult(name, false, "SncConsumer: no files to consume in: " + directory);
-        //TODO: continue with fileName == expected
-        //Mock
-        return new TestResult();
+        if(!directory.isDirectory()) {
+            return new TestResult(name, false, "SncConsumer: Directory not found " + directory);
+        }
+
+        for(File f : directory.listFiles()) {
+            if(f.getName().startsWith(expectedValue)){
+                f.delete();
+                return new TestResult(name, true, "Found expected file " + f.getAbsolutePath());
+            }
+        }
+
+        return new TestResult(name, false, "not found expected file with name " + expectedValue);
     }
 }
