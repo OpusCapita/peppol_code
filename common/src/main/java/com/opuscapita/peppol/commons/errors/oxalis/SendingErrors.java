418e01851a46303910765ea7764cc2b1339cf588
rozeser1 <sergejs.roze@opuscapita.com>
2017-09-12 15:09:11 +0300

    Made retry in outbound optional depending on the error type

--- common/src/main/java/com/opuscapita/peppol/commons/errors/oxalis/SendingErrors.java
+++ common/src/main/java/com/opuscapita/peppol/commons/errors/oxalis/SendingErrors.java
@@ -8,17 +8,27 @@ package com.opuscapita.peppol.commons.errors.oxalis;
 public enum SendingErrors {
 
     /** Error in input data */
-    DATA_ERROR,
+    DATA_ERROR(false),
     /** Failed to connect for some reason */
-    CONNECTION_ERROR,
+    CONNECTION_ERROR(true),
     /** Receiving AP returned code other than 200 */
-    RECEIVING_AP_ERROR,
+    RECEIVING_AP_ERROR(true),
     /** Unknown recipient or unsupported data format, currently not distinguishable */
-    UNKNOWN_RECIPIENT,
-    UNSUPPORTED_DATA_FORMAT,
+    UNKNOWN_RECIPIENT(false),
+    UNSUPPORTED_DATA_FORMAT(false),
     /** Security issue - expired, invalid, or unknown certificates */
-    SECURITY_ERROR,
+    SECURITY_ERROR(true),
     /** All other errors */
-    OTHER_ERROR
+    OTHER_ERROR(false);
+
+    private boolean temporary;
+
+    SendingErrors(boolean temporary) {
+        this.temporary = temporary;
+    }
+
+    public boolean isTemporary() {
+        return temporary;
+    }
 
 }
