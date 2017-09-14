54b11f16cf91501d1a25322c8e9e865aff83a10c
Daniil Kalnin <daniil.kalnin@opuscapita.com>
2017-09-14 09:00:21 +0300

    Unit test added for checks on SBDH fields.

--- common/src/test/java/com/opuscapita/peppol/commons/container/xml/DocumentParserTest.java
+++ common/src/test/java/com/opuscapita/peppol/commons/container/xml/DocumentParserTest.java
@@ -4,6 +4,8 @@ import com.opuscapita.peppol.commons.container.DocumentInfo;
 import com.opuscapita.peppol.commons.container.document.Archetype;
 import com.opuscapita.peppol.commons.container.process.route.Endpoint;
 import com.opuscapita.peppol.commons.container.process.route.ProcessType;
+import io.vavr.collection.List;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +24,7 @@ import static org.junit.Assert.*;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations="classpath:application.yml")
+@TestPropertySource(locations = "classpath:application.yml")
 public class DocumentParserTest {
     @Autowired
     private DocumentTemplates templates;
@@ -31,9 +33,7 @@ public class DocumentParserTest {
 
     @Test
     public void testNewFields() throws Exception {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/BIIXY_document_type_test.xml")) {
             DocumentInfo result = parser.parse(inputStream, "/valid/BIIXY_document_type_test.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -47,9 +47,7 @@ public class DocumentParserTest {
 
     @Test
     public void testParseValid() throws Exception {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/ehf.xml")) {
             DocumentInfo result = parser.parse(inputStream, "ehf.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -79,9 +77,7 @@ public class DocumentParserTest {
 
     @Test
     public void testParseAlmostValid() throws Exception {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/invalid/ehf_no_issue_date.xml")) {
             DocumentInfo result = parser.parse(inputStream, "ehf_no_issue_date.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -108,12 +104,11 @@ public class DocumentParserTest {
         }
 
     }
+
     @SuppressWarnings("Duplicates")
     @Test
     public void testParseEhfInvoiceBiiyx() throws Exception {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/BIIXY_document_type_test.xml")) {
             DocumentInfo result = parser.parse(inputStream, "BIIXY_document_type_test.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -142,9 +137,7 @@ public class DocumentParserTest {
 
     @Test
     public void testParseEhfCatalogue() throws Exception {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/Catalogue_document_type_test.xml")) {
             DocumentInfo result = parser.parse(inputStream, "Catalogue_document_type_test.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -170,9 +163,7 @@ public class DocumentParserTest {
 
     @Test
     public void testParseEhfCatalogueSellerSuplier() throws Exception {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/Catalogue_document_type_seller_supplier_test.xml")) {
             DocumentInfo result = parser.parse(inputStream, "Catalogue_document_type_seller_supplier_test.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -197,10 +188,8 @@ public class DocumentParserTest {
     }
 
     @Test
-    public void testParseSvefakturaNoSbdh() throws Exception{
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+    public void testParseSvefakturaNoSbdh() throws Exception {
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/invalid/SFTI_svefaktura_BasicInvoice-1.0_Invoice-no-sbdh.xml")) {
             DocumentInfo result = parser.parse(inputStream, "SFTI_svefaktura_BasicInvoice-1.0_Invoice-no-sbdh.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -210,10 +199,8 @@ public class DocumentParserTest {
     }
 
     @Test
-    public void testParseSvefakturaWrongPartyId() throws Exception{
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+    public void testParseSvefakturaWrongPartyId() throws Exception {
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/invalid/SFTI_svefaktura_BasicInvoice-1.0_Invoice-SBDH-senderID-different-SellerParty.xml")) {
             DocumentInfo result = parser.parse(inputStream, "SFTI_svefaktura_BasicInvoice-1.0_Invoice-SBDH-senderID-different-SellerParty.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -223,10 +210,8 @@ public class DocumentParserTest {
     }
 
     @Test
-    public void testParseSvefakturaValid() throws Exception{
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+    public void testParseSvefakturaValid() throws Exception {
+        DocumentParser parser = createDocumentParser();
 
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/SFTI_svefaktura_BasicInvoice-1.0_Invoice.xml")) {
             DocumentInfo result = parser.parse(inputStream, "SFTI_svefaktura_BasicInvoice-1.0_Invoice.xml", new Endpoint("test", ProcessType.TEST), shouldFailOnInconsistency);
@@ -235,19 +220,54 @@ public class DocumentParserTest {
         }
     }
 
-    @Test
-    public void testParseNetClientSender() throws Exception{
+    @NotNull
+    private DocumentParser createDocumentParser() {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+        return new DocumentParser(factory, templates);
+    }
+
+    @Test
+    public void testMissingSbdhFields() throws Exception {
+        DocumentParser parser = createDocumentParser();
+        try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/invalid/ehf_no_sbdh_fields.xml")) {
+            DocumentInfo result = parser.parse(inputStream, "ehf_no_sbdh_fields.xml", new Endpoint("test", ProcessType.UNKNOWN), false);
+            assertNotNull(result);
+            assertEquals(6, result.getErrors().size());
+            List<String> expectedErrorMessages = List.of(
+                    "Invalid or missing sender id",
+                    "Invalid or missing recipient id",
+                    "SBDH is missing sender_id field",
+                    "SBDH is missing recipient_id field",
+                    "PROCESSID is missing or empty",
+                    "DOCUMENTID is missing or empty"
+            );
+            long matchedErrors = result
+                    .getErrors()
+                    .stream()
+                    .map(error -> error.getMessage())
+                    .filter(errorMessage -> expectedErrorMessages
+                            .toStream()
+                            .map(expectedErrorMessage -> errorMessage.contains(expectedErrorMessage))
+                            .count(matchingMessage -> matchingMessage) > 0
+                    )
+                    .count();
+            assertEquals(expectedErrorMessages.size(), matchedErrors);
+            result.getErrors().forEach(error -> System.out.println(error.getMessage()));
+        }
+    }
+
+    @Test
+    public void testParseNetClientSender() throws Exception {
+        DocumentParser parser = createDocumentParser();
 
         //consistent check
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/special_sender_id/netclient_as_sender.xml")) {
             DocumentInfo result = parser.parse(inputStream, "netclient_as_sender.xml", new Endpoint("test", ProcessType.TEST), true);
             assertNotNull(result);
             assertTrue(result.getErrors().get(0).getMessage().contains(
-                        "There are different conflicting values in the document for the field 'sender_id: [9908:995483254, 991723145, 991723145]"
-                ));
+                    "There are different conflicting values in the document for the field 'sender_id: [9908:995483254, 991723145, 991723145]"
+            ));
             assertEquals(Archetype.INVALID, result.getArchetype());
         }
         //inconsistent check
@@ -261,16 +281,14 @@ public class DocumentParserTest {
 
     @Test
     @SuppressWarnings("Duplicates")
-    public void testParseMvaSenderId() throws Exception{
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+    public void testParseMvaSenderId() throws Exception {
+        DocumentParser parser = createDocumentParser();
         //consistent check
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/special_sender_id/9908_989170325MVA-9908_890164072-20170825.xml")) {
             DocumentInfo result = parser.parse(inputStream, "9908_989170325MVA-9908_890164072-20170825.xml", new Endpoint("test", ProcessType.TEST), true);
             assertNotNull(result);
             assertTrue(result.getErrors().get(0).getMessage().contains(
-                        "There are different conflicting values in the document for the field 'sender_id: [9908:989170325MVA, 989170325, 989170325]"
+                    "There are different conflicting values in the document for the field 'sender_id: [9908:989170325MVA, 989170325, 989170325]"
             ));
             assertEquals(Archetype.INVALID, result.getArchetype());
         }
@@ -284,10 +302,8 @@ public class DocumentParserTest {
     }
 
     @Test
-    public void testParseSiNoSbdh() throws Exception{
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentParser parser = new DocumentParser(factory, templates);
+    public void testParseSiNoSbdh() throws Exception {
+        DocumentParser parser = createDocumentParser();
         //consistent check
         try (InputStream inputStream = DocumentParserTest.class.getResourceAsStream("/valid/simpler_invoicing_files/Valid5_no_sbdh.xml")) {
             DocumentInfo result = parser.parse(inputStream, "Valid5_no_sbdh.xml", new Endpoint("test", ProcessType.TEST), true);
