99dfb0121e73b2094845532d2c998ea28ec22fc6
Sergejs Gamans <sergejs.gamans@opuscapita.com>
2017-10-13 18:51:34 +0300

    added null check

--- common/src/main/java/com/opuscapita/peppol/commons/container/process/route/EndpointUtil.java
+++ common/src/main/java/com/opuscapita/peppol/commons/container/process/route/EndpointUtil.java
@@ -3,6 +3,9 @@ package com.opuscapita.peppol.commons.container.process.route;
 public class EndpointUtil {
     public static boolean isTerminal(Endpoint endpoint) {
         boolean result = false;
+        if (endpoint == null) {
+            return false;
+        }
         switch (endpoint.getType()) {
             case OUT_OUTBOUND:
             case OUT_PEPPOL_FINAL:
