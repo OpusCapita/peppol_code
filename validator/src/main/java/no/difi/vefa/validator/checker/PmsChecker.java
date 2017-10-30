762b6bc9b2c1ff4b0225ddc862e5cb41fa16971c
Daniil Kalnin <daniil.kalnin@opuscapita.com>
2017-10-30 07:48:15 +0200

    Backported workaround for UNKNOWN business rule when validation fails on EHF rules.

--- validator/src/main/java/no/difi/vefa/validator/checker/PmsChecker.java
+++ validator/src/main/java/no/difi/vefa/validator/checker/PmsChecker.java
@@ -73,6 +73,8 @@ public class PmsChecker implements Checker {
             assertionType.setIdentifier("UNKNOWN");
         }
 
+
+        //Workaround backported from Vefa validator github repo
         if(failedAssert.getId() != null) {
             assertionType.setIdentifier(failedAssert.getId());
         }
