02a89d5308b39b00a20355f0e84d3ba8764cd9d9
rozeser1 <Klop0317>
2018-03-25 11:40:27 +0300

    Re-enabled unit tests for validator, still some work to do but at least something

--- validator/src/test/java/com/opuscapita/peppol/validator/controller/ValidationControllerTest.java
+++ validator/src/test/java/com/opuscapita/peppol/validator/controller/ValidationControllerTest.java
@@ -9,7 +9,6 @@ import com.opuscapita.peppol.commons.container.process.route.Endpoint;
 import com.opuscapita.peppol.validator.ValidationController;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +37,6 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest(classes = ValidationControllerTestConfig.class)
 @EnableConfigurationProperties
 public class ValidationControllerTest {
-    private final static String[] PROFILES = { "difi", "svefaktura1" };
-
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private ValidationController controller;
@@ -48,7 +45,6 @@ public class ValidationControllerTest {
     private DocumentLoader documentLoader;
 
     @Test
-    @Ignore("work in progress")
     public void goThroughTestMaterials() throws Exception {
         File testFiles = new File(ValidationControllerTest.class.getResource("/test-materials").getFile());
         processDirectory(testFiles);
@@ -83,8 +79,8 @@ public class ValidationControllerTest {
     // an assumption is made that file is formatted
     private List<String> getExpected(@NotNull File file) throws IOException {
         List<String> lines = FileUtils.readLines(file, Charset.defaultCharset());
-        List<String> expected = getExpected(lines, new ArrayList<>(), "Errors:", "E: ");
-        return getExpected(lines, expected, "Warnings:", "W: ");
+        List<String> expected = getExpected(lines, new ArrayList<>(), "Error", "E: ");
+        return getExpected(lines, expected, "Warning", "W: ");
     }
 
     private List<String> getExpected(@NotNull List<String> lines, @NotNull List<String> expected, @NotNull String header, @NotNull String prefix) {
@@ -93,7 +89,7 @@ public class ValidationControllerTest {
         for (String line : lines) {
             line = line.trim();
             if (reading) {
-                if (line.equals("")) {
+                if (line.equals("") || "none".equalsIgnoreCase(line) || "-->".equals(line)) {
                     return expected;
                 }
                 if (line.contains(" x ")) {
@@ -108,31 +104,13 @@ public class ValidationControllerTest {
                 }
             }
 
-            if (header.equals(line)) {
+            if (line.startsWith(header)) {
                 reading = true;
             }
         }
 
         return expected;
     }
-
-
-
-//    @SuppressWarnings("ConstantConditions")
-//    @Ignore
-//    @Test
-//    public void betterValidation() throws Exception {
-//        File dir = new File(ValidationControllerTest.class.getResource("/test-materials").getFile());
-//        for (String file : dir.list((d, n) -> n.endsWith(".xml"))) {
-//            String path = dir.getAbsolutePath() + File.separator + file;
-//            List<String> expected = getExpected(path);
-//
-//            ContainerMessage cm = loadDocument(path);
-//            cm = controller.validate(cm);
-//
-//            assertTrue(compare(cm, expected));
-//        }
-//    }
 
     @SuppressWarnings("ConstantConditions")
     private boolean compare(@NotNull ContainerMessage cm, @NotNull List<String> expected) {
@@ -165,62 +143,10 @@ public class ValidationControllerTest {
         return passed;
     }
 
-//    private List<String> getExpected(@NotNull String file) throws IOException {
-//        return Files.lines(new File(file).toPath())
-//                .map(String::trim)
-//                .filter(l -> l.startsWith("W: ") || l.startsWith("E: "))
-//                .collect(Collectors.toList());
-//    }
-
-//    @Test
-//    public void validate() throws Exception {
-//        for (String profile : PROFILES) {
-//            testProfile(profile);
-//        }
-//    }
-
-//    @SuppressWarnings("ConstantConditions")
-//    private void testProfile(String profile) throws Exception {
-//        File dir = new File(new File(ValidationControllerTest.class.getResource("/test_data").getFile()), profile + "_files");
-//        FilenameFilter filter = (dir1, name) -> name.toLowerCase().endsWith(".xml") && name.contains("alid");
-//        for (String file : dir.list(filter)) {
-//            testFile(dir.getAbsolutePath() + File.separator + file);
-//        }
-//    }
-//
-//    @SuppressWarnings("ConstantConditions")
-//    private void testFile(String file) throws Exception {
-//        ContainerMessage cm = loadDocument(file);
-//        try {
-//            cm = controller.validate(cm);
-//        } catch (Exception e) {
-//            if (cm.getFileName().contains("invalid")) {
-//                return;
-//            } else {
-//                fail("File " + cm.getFileName() + " has no invalid in file name but validation failed with exception:\n" + e.getMessage());
-//            }
-//        }
-//        if (file.toLowerCase().contains("invalid")) {
-//            assertTrue("Document " + file + " contains 'invalid' in the file name but the error is not found", cm.hasErrors());
-//        } else {
-//            assertFalse("Document " + file + " has no 'invalid' in the file name but failed validation: " + getErrors(cm),
-//                    cm.hasErrors() || cm.getDocumentInfo().getArchetype() == Archetype.INVALID);
-//        }
-//    }
-
     private ContainerMessage loadDocument(String file) throws Exception {
         DocumentInfo info = documentLoader.load(file, Endpoint.TEST);
         System.out.println("Document " + file + " recognized as " + info.getArchetype());
         return new ContainerMessage("meatdata", file, Endpoint.TEST).setDocumentInfo(info);
     }
-
-//    @SuppressWarnings("ConstantConditions")
-//    private String getErrors(ContainerMessage cm) {
-//        StringBuilder result = new StringBuilder();
-//        for (DocumentError error : cm.getDocumentInfo().getErrors()) {
-//            result.append("\n\t").append(error);
-//        }
-//        return result.toString();
-//    }
 
 }
