f0fdfbf5f2f82ffe2eed4f8047ed9d253ab58313
Daniil Kalnin <daniil.kalnin@opuscapita.com>
2018-06-01 16:09:04 +0300

    Fix for insufficient logging in case of document type recognition failure.

--- preprocessing/src/main/java/com/opuscapita/peppol/preprocessing/PreprocessingApp.java
+++ preprocessing/src/main/java/com/opuscapita/peppol/preprocessing/PreprocessingApp.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.opuscapita.peppol.commons.container.ContainerMessage;
 import com.opuscapita.peppol.commons.container.document.Archetype;
+import com.opuscapita.peppol.commons.container.document.DocumentError;
 import com.opuscapita.peppol.commons.errors.ErrorHandler;
 import com.opuscapita.peppol.commons.events.EventingMessageUtil;
 import com.opuscapita.peppol.commons.mq.MessageQueue;
@@ -25,6 +26,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
 import java.io.File;
+import java.util.stream.Collectors;
 
 @SpringBootApplication(scanBasePackages = {"com.opuscapita.peppol.commons", "com.opuscapita.peppol.preprocessing"})
 @EnableDiscoveryClient
@@ -87,8 +89,11 @@ public class PreprocessingApp {
         }
         if (cm.getDocumentInfo() != null && cm.getDocumentInfo().getArchetype() == Archetype.UNRECOGNIZED) {
             String fileName = new File(cm.getFileName()).getName();
+            String errorsSoFar = cm.getDocumentInfo().getErrors().stream().map(DocumentError::toString).collect(Collectors.joining("\n\r"));
+            logger.warn("Document type recognition for " + fileName + " failed with following errors: " + errorsSoFar);
             errorHandler.reportWithContainerMessage(cm, null, "Document not recognized by the parser in: " + fileName);
             cm.setStatus(cm.getProcessingInfo().getCurrentEndpoint(), "invalid file, document type unrecognized");
+
         } else if (cm.getDocumentInfo() == null || cm.getDocumentInfo().getArchetype() == Archetype.INVALID) {
             cm.setStatus(cm.getProcessingInfo().getCurrentEndpoint(), "invalid file");
             EventingMessageUtil.reportEvent(cm, "Invalid file");
