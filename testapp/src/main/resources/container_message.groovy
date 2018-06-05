421bb9473aca30df6f024e201eacd8c8e98b45f0
rozeser1 <Klop0317>
2018-06-05 13:44:27 +0300

    Removed obsolete testapp module

--- testapp/src/main/resources/container_message.groovy
+++
@@ -1,7 +0,0 @@
-import com.opuscapita.peppol.commons.container.document.DocumentLoader
-import com.opuscapita.peppol.commons.container.document.impl.UblDocument
-
-def cm = new DocumentLoader().load("/tmp/cm_input.xml")
-
-print cm instanceof UblDocument
-
