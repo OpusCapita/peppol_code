e4effd11c468774eb7b39f47cd8e9451555c3a23
rozeser1 <Klop0317>
2018-11-13 12:45:23 +0200

    Changed to meaningful notification

--- support-ui/src/main/java/com/opuscapita/peppol/support/ui/controller/ApplicationController.java
+++ support-ui/src/main/java/com/opuscapita/peppol/support/ui/controller/ApplicationController.java
@@ -44,7 +44,19 @@ public class ApplicationController {
             User user = (User) authentication.getPrincipal();
             map.addAttribute("user", user);
             map.addAttribute("customerListEnabled", isCustomerListEnabled());
-            map.addAttribute("customerListMessage", isCustomerListEnabled() ? "" : "Customer e-mail sending disabled because of reasons");
+            map.addAttribute("customerListMessage", isCustomerListEnabled() ? "" :
+                    "Dear user,<br/>" +
+                            "The communication logic of outbound related error messages has changed.<br/>" +
+                            "Instead of sending direct email to invoice sender our Access Point informs business platform (XIB, A2A or Sirius)" +
+                            " about negative acknowledgment (MLR). Customer communication is now executed by A2A gateways and" +
+                            " XIB WWD like in case of any other negative acknowledgment.<br/>" +
+                            "Displayed email addresses in this webpage were synced with business platforms before the change." +
+                            " This email list shouldn’t be updated anymore and customer contacts are now maintained in respective" +
+                            " business platform.<br/>" +
+                            "<br/>" +
+                            "NB! Inbound related error message handling remains currently the same." +
+                            " We have a plan to change that as well – error messages will be delivered to sending access point" +
+                            " instead of customer (we will notify you about the upcoming change).");
 
             boolean admin = false;
             for (Role role : user.getRoles()) {
