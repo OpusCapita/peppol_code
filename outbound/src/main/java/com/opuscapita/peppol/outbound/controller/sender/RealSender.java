57b8feb02c787eea79f97cd81b8e4c5f7ff2d7ec
thebilge <ibrahimbilge7@gmail.com>
2019-02-22 10:40:22 +0100

    outbound sender refactor

--- outbound/src/main/java/com/opuscapita/peppol/outbound/controller/sender/RealSender.java
+++ outbound/src/main/java/com/opuscapita/peppol/outbound/controller/sender/RealSender.java
@@ -2,8 +2,6 @@ package com.opuscapita.peppol.outbound.controller.sender;
 
 import com.opuscapita.peppol.commons.container.ContainerMessage;
 import com.opuscapita.peppol.commons.container.DocumentInfo;
-import com.opuscapita.peppol.commons.container.document.Archetype;
-import com.opuscapita.peppol.outbound.util.OxalisUtils;
 import no.difi.oxalis.api.lang.OxalisContentException;
 import no.difi.oxalis.api.lang.OxalisTransmissionException;
 import no.difi.oxalis.api.outbound.TransmissionRequest;
@@ -11,17 +9,12 @@ import no.difi.oxalis.api.outbound.TransmissionResponse;
 import no.difi.oxalis.api.outbound.Transmitter;
 import no.difi.oxalis.outbound.OxalisOutboundComponent;
 import no.difi.oxalis.outbound.transmission.TransmissionRequestBuilder;
-import no.difi.vefa.peppol.common.model.ParticipantIdentifier;
-import no.difi.vefa.peppol.common.model.ProcessIdentifier;
-import no.difi.vefa.peppol.common.model.Scheme;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -49,7 +42,6 @@ public class RealSender implements PeppolSender {
         return oxalisOutboundModule.getTransmissionRequestBuilder();
     }
 
-    @SuppressWarnings("unused")
     @NotNull
     public TransmissionResponse send(@NotNull ContainerMessage cm) throws IOException, OxalisContentException, OxalisTransmissionException {
         DocumentInfo document = cm.getDocumentInfo();
@@ -58,25 +50,12 @@ public class RealSender implements PeppolSender {
         }
 
         try (InputStream inputStream = new FileInputStream(cm.getFileName())) {
-            TransmissionRequest transmissionRequest = getTransmissionRequestBuilder()
-                    .documentType(OxalisUtils.getPeppolDocumentTypeId(cm))
-//                    .processType(ProcessIdentifier.of(document.getProfileId(), getProcessIdentifierScheme(cm)))
-                    .sender(ParticipantIdentifier.of(document.getSenderId()))
-                    .receiver(ParticipantIdentifier.of(document.getRecipientId()))
-                    .payLoad(inputStream)
-                    .build();
+            TransmissionRequest transmissionRequest = getTransmissionRequestBuilder().payLoad(inputStream).build();
 
             logger.info("Thread " + Thread.currentThread().getName() + " is about to send message: " + cm.toLog() + " to endpoint: " + transmissionRequest.getEndpoint());
 
             Transmitter transmitter = oxalisOutboundModule.getTransmitter();
             return transmitter.transmit(transmissionRequest);
         }
-    }
-
-    private Scheme getProcessIdentifierScheme(@NotNull ContainerMessage cm) {
-        if (Archetype.SVEFAKTURA1.equals(cm.getDocumentInfo().getArchetype())) {
-            return Scheme.of("sfti-procid");
-        }
-        return ProcessIdentifier.DEFAULT_SCHEME;
     }
 }
