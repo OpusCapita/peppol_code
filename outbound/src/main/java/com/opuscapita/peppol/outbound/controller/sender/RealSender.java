925f771a8c456df250ae7ac9bfd55e12de5cbf4e
thebilge <ibrahimbilge7@gmail.com>
2019-02-26 10:19:00 +0100

    outbound sender refactor

--- outbound/src/main/java/com/opuscapita/peppol/outbound/controller/sender/RealSender.java
+++ outbound/src/main/java/com/opuscapita/peppol/outbound/controller/sender/RealSender.java
@@ -2,6 +2,7 @@ package com.opuscapita.peppol.outbound.controller.sender;
 
 import com.opuscapita.peppol.commons.container.ContainerMessage;
 import com.opuscapita.peppol.commons.container.DocumentInfo;
+import com.opuscapita.peppol.outbound.util.OxalisUtils;
 import no.difi.oxalis.api.lang.OxalisContentException;
 import no.difi.oxalis.api.lang.OxalisTransmissionException;
 import no.difi.oxalis.api.outbound.TransmissionRequest;
@@ -9,6 +10,10 @@ import no.difi.oxalis.api.outbound.TransmissionResponse;
 import no.difi.oxalis.api.outbound.Transmitter;
 import no.difi.oxalis.outbound.OxalisOutboundComponent;
 import no.difi.oxalis.outbound.transmission.TransmissionRequestBuilder;
+import no.difi.vefa.peppol.common.model.ParticipantIdentifier;
+import no.difi.vefa.peppol.common.model.ProcessIdentifier;
+import no.difi.vefa.peppol.common.model.Scheme;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,12 +55,33 @@ public class RealSender implements PeppolSender {
         }
 
         try (InputStream inputStream = new FileInputStream(cm.getFileName())) {
-            TransmissionRequest transmissionRequest = getTransmissionRequestBuilder().payLoad(inputStream).build();
+            TransmissionRequestBuilder transmissionRequestBuilder = getTransmissionRequestBuilder()
+                    .documentType(OxalisUtils.getPeppolDocumentTypeId(cm))
+                    .processType(OxalisUtils.getPeppolProcessTypeId(cm, ProcessIdentifier.DEFAULT_SCHEME))
+                    .sender(ParticipantIdentifier.of(document.getSenderId()))
+                    .receiver(ParticipantIdentifier.of(document.getRecipientId()))
+                    .payLoad(inputStream);
 
+            TransmissionRequest transmissionRequest = transmissionRequestBuilder.build();
+            Transmitter transmitter = oxalisOutboundModule.getTransmitter();
             logger.info("Thread " + Thread.currentThread().getName() + " is about to send message: " + cm.toLog() + " to endpoint: " + transmissionRequest.getEndpoint());
 
-            Transmitter transmitter = oxalisOutboundModule.getTransmitter();
-            return transmitter.transmit(transmissionRequest);
+            try {
+                return transmitter.transmit(transmissionRequest);
+
+            } catch (Exception e) {
+                /**
+                 * Temp workaround for process id protocol issue, need to find an exact way to determine correct protocol.
+                 */
+                String message = e.getMessage();
+                if (StringUtils.isNotBlank(message) &&
+                        message.replaceAll("\n", " ").replaceAll("\r", " ").contains("transport profile(s) not found")) {
+                    transmissionRequest = transmissionRequestBuilder.processType(OxalisUtils.getPeppolProcessTypeId(cm, Scheme.of("sfti-procid"))).build();
+                    return transmitter.transmit(transmissionRequest);
+                } else {
+                    throw e;
+                }
+            }
         }
     }
 }
--- outbound/src/main/java/com/opuscapita/peppol/outbound/util/OxalisUtils.java
+++ outbound/src/main/java/com/opuscapita/peppol/outbound/util/OxalisUtils.java
@@ -6,6 +6,7 @@ import com.opuscapita.peppol.commons.container.metadata.PeppolMessageMetadata;
 import no.difi.oxalis.sniffer.identifier.CustomizationIdentifier;
 import no.difi.oxalis.sniffer.identifier.PeppolDocumentTypeId;
 import no.difi.vefa.peppol.common.model.DocumentTypeIdentifier;
+import no.difi.vefa.peppol.common.model.ProcessIdentifier;
 import no.difi.vefa.peppol.common.model.Scheme;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +17,16 @@ import org.jetbrains.annotations.NotNull;
  * @author Sergejs.Roze
  */
 public class OxalisUtils {
+
+    public static ProcessIdentifier getPeppolProcessTypeId(@NotNull ContainerMessage cm, Scheme scheme) {
+        PeppolMessageMetadata metadata = cm.getProcessingInfo().getPeppolMessageMetadata();
+        if (metadata != null) {
+            if (StringUtils.isNotBlank(metadata.getProfileTypeIdentifier())) {
+                return ProcessIdentifier.of(metadata.getProfileTypeIdentifier(), scheme);
+            }
+        }
+        return ProcessIdentifier.of(cm.getDocumentInfo().getProfileId(), scheme);
+    }
 
     public static DocumentTypeIdentifier getPeppolDocumentTypeId(@NotNull ContainerMessage cm) {
         PeppolMessageMetadata metadata = cm.getProcessingInfo().getPeppolMessageMetadata();
